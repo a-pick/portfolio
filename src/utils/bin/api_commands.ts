6cbbfb1283f33ef16e9d88ca47906d393ae58cad
alex <94504311+a-pick@users.noreply.github.com>
2022-05-28 22:04:27 -0600

    Add files via upload

---
+++ src/utils/bin/api_commands.ts
@@ -0,0 +1,41 @@
+// // List of commands that require API calls
+
+import { getProjects } from '../api';
+import { getQuote } from '../api';
+import { getReadme } from '../api';
+import { getWeather } from '../api';
+
+export const projects = async (args: string[]): Promise<string> => {
+  const projects = await getProjects();
+  return projects
+    .map(
+      (repo) =>
+        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
+    )
+    .join('\n');
+};
+
+export const quote = async (args: string[]): Promise<string> => {
+  const quotes = [
+    "Alex is an awesome developer. And a great son. \n- Alex's Mom",
+    "You should really hire my son. \n- Alex's Dad",
+    "Don't hire my brother. \n- Alex's Sister",
+  ];
+  const rand = Math.floor(Math.random() * quotes.length);
+  return quotes[rand];
+};
+
+export const readme = async (args: string[]): Promise<string> => {
+  const readme = await getReadme();
+  return `Opening GitHub README...\n
+  ${readme}`;
+};
+
+export const weather = async (args: string[]): Promise<string> => {
+  const city = args.join('+');
+  if (!city) {
+    return 'Usage: weather [city]. Example: weather edmonton';
+  }
+  const weather = await getWeather(city);
+  return weather;
+};
